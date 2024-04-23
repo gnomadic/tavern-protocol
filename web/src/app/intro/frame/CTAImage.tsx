123f5e53919fd14fee6d16881ebe60789adadcf5
gnomadic <edflem+github@gmail.com>
2024-04-23 13:39:55 -0400

    chore: updating images for frames

--- web/src/app/intro/frame/CTAImage.tsx
+++ web/src/app/intro/frame/CTAImage.tsx
@@ -3,50 +3,46 @@ export default function CTAImage() {
   return (
 
     <div
-      style={{
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#000',
-        fontSize: 32,
-        fontWeight: 600,
-        color: "#fff",
-        backgroundImage: 'linear-gradient(to right, #334d50, #cbcaa5)',
-
-      }}
-    >
-
-      <div style={{ display: 'flex', marginTop: 0, flexDirection: 'row', flexWrap: 'wrap' }}>
-        <div style={{ flexBasis: '50%', justifyContent: 'flex-end', color: '#000' }}>create your game</div>
-        <div style={{ flexBasis: '50%', justifyContent: 'flex-start', paddingLeft: 15 }}>with a game gactory</div>
-      </div>
-
-
-      <div style={{ display: 'flex', marginTop: 20, flexDirection: 'row', flexWrap: 'wrap' }}>
-        <div style={{ flexBasis: '50%', justifyContent: 'flex-end', color: '#000' }}>choose your modules</div>
-        <div style={{ flexBasis: '50%', justifyContent: 'flex-start', paddingLeft: 15 }}>from the registry</div>
-      </div>
-
-      <div style={{ display: 'flex', marginTop: 20, flexDirection: 'row', flexWrap: 'wrap' }}>
-        <div style={{ flexBasis: '50%', justifyContent: 'flex-end', color: '#000' }}>configure your modules</div>
-        <div style={{ flexBasis: '50%', justifyContent: 'flex-start', paddingLeft: 15 }}>with entities</div>
-      </div>
-
-      <div style={{ display: 'flex', marginTop: 20, flexDirection: 'row', flexWrap: 'wrap' }}>
-        <div style={{ flexBasis: '50%', justifyContent: 'flex-end', color: '#000' }}>combine module functions</div>
-        <div style={{ flexBasis: '50%', justifyContent: 'flex-start', paddingLeft: 15 }}>into game gunctions</div>
-      </div>
-
-      <div style={{ display: 'flex', marginTop: 20, flexDirection: 'row', flexWrap: 'wrap' }}>
-        <div style={{ flexBasis: '50%', justifyContent: 'flex-end', color: '#000' }}></div>
-        <div style={{ flexBasis: '50%', color: `#0`, justifyContent: 'flex-start', paddingLeft: 15 }}>play </div>
-      </div>
-
+    style={{
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#000',
+      fontSize: 32,
+      fontWeight: 600,
+      color: "#fff",
+      backgroundImage: 'linear-gradient(to right, #334d50, #cbcaa5)',
+  
+    }}
+  >
+  
+    <div style={{ display: 'flex', marginTop: 0, flexDirection: 'row', flexWrap: 'wrap' }}>
+      <div style={{ flexBasis: '50%', justifyContent: 'flex-end', color: '#000' }}>If you are interested</div>
+      <div style={{ flexBasis: '50%', justifyContent: 'flex-start', paddingLeft: 15 }}>follow us</div>
     </div>
-
+  
+       <div style={{ display: 'flex', marginTop: 20, flexDirection: 'row', flexWrap: 'wrap' }}>
+      <div style={{ flexBasis: '50%', justifyContent: 'flex-end', color: '#000' }}></div>
+      <div style={{ flexBasis: '50%', justifyContent: 'flex-start', paddingLeft: 15 }}>share your ideas for games</div>
+    </div>
+  
+         <div style={{ display: 'flex', marginTop: 20, flexDirection: 'row', flexWrap: 'wrap' }}>
+      <div style={{ flexBasis: '50%', justifyContent: 'flex-end', color: '#000' }}></div>
+      <div style={{ flexBasis: '50%', justifyContent: 'flex-start', paddingLeft: 15 }}>share your ideas for modules</div>
+    </div>
+  
+  
+  
+           <div style={{ display: 'flex', marginTop: 20, flexDirection: 'row', flexWrap: 'wrap' }}>
+      <div style={{ flexBasis: '50%', justifyContent: 'flex-end', color: '#000' }}></div>
+      <div style={{ flexBasis: '50%', color:`#0`, justifyContent: 'flex-start', paddingLeft: 15 }}>help us find a name </div>
+    </div>
+  
+  </div>
+  
 
   )
 }

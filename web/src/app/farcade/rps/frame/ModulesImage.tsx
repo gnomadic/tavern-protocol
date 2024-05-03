e107737c67d2aeb9095736fa3ca7272a4df7ae2e
gnomadic <edflem+github@gmail.com>
2024-05-03 13:53:49 -0400

    chore: fixing build

---
+++ web/src/app/farcade/rps/frame/ModulesImage.tsx
@@ -0,0 +1,91 @@
+export default function ModulesImage() {
+
+  return (
+
+
+    <div
+    style={{
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#000',
+      fontSize: 32,
+      fontWeight: 600,
+      color: "#fff",
+      backgroundImage: 'linear-gradient(to right, #334d50, #cbcaa5)',
+  
+    }}
+  >
+  
+  
+  
+  <div style={{display: 'flex', marginTop: 0, flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end', color: '#000'  }}>building blocks for </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15}}>time-based activity</div>
+      
+    </div>
+  
+    <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end' }}> </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15}}>hosted multiplayer</div>
+    </div>
+  
+      <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end' }}> </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15}}>player roles/classes</div>
+    </div>
+  
+        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end' }}> </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15}}>mmo sessions</div>
+    </div>
+  
+          <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end' }}> </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15}}>deck of cards </div>
+    </div>
+  
+  
+          <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end' }}> </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15}}>player position </div>
+    </div>
+  
+  
+          <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end' }}> </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15}}>neighbor interactions </div>
+    </div>
+  
+  
+  
+  
+  
+          <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end' }}> </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15}}>token rewards </div>
+    </div>
+  
+            <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end' }}> </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15}}>nft gating</div>
+    </div>
+  
+              <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <div style={{ flexBasis: '43%', justifyContent:'flex-end' }}> </div>
+      <div style={{ flexBasis: '57%' ,justifyContent:'flex-start', paddingLeft: 15, color:'#000'}}>composable game functionality</div>
+    </div>
+  
+  
+  
+   
+  
+      
+    
+  </div>
+  
+  )
+}

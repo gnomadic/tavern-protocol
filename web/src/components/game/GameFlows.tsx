13512db48dc836bb5659dc04afc5bade0773f828
gnomadic <edflem+github@gmail.com>
2024-06-25 10:01:23 -0400

    flow tab:

--- web/src/components/game/GameFlows.tsx
+++ web/src/components/game/GameFlows.tsx
@@ -26,52 +26,52 @@ export default function GameFlows(props: StepThreeProps) {
 
     return (
         <section id='connect' className=' font-outfit'>
-        <div className='p-4 text-2xl'>
-            Flows
-        </div>
-    
-            <section>
-                {/* <SmallTitle title={'flows/' + summary?.flows.length} /> */}
-                <ul className='py-12 md:py-24'>
-                    {Array.from({ length: summary?.flows.length as number }).map((object, i) => {
-                        return (
-                            <div key={i} className='justify-center pt-12'>
-                                <div className='pt-5 pl-5 text-lg border-b-2 border-white'>
-                                    {i + 1}{'/'}{summary!.flows[i].name}{' '}
-                                </div>
+            <div className='p-4 text-2xl'>
+                Flows
+            </div>
 
-                                <div className="mx-12 py-8">
-                                    <div className="border-b-2 border-white text-xs ">
-                                        component function
-                                    </div>
-
-                                    {Array.from({ length: summary!.flows[i].values.length as number }).map((object, j) => {
-                                        return (
-                                            <p key={j}>
-                                                {j + 1}/
-                                                <span>
-                                                    <a target="_blank"
-                                                        rel="noopener noreferrer"
-                                                        href={deploy.scan + summary!.flows[i].values[j].value} >
-                                                        {pretty(summary!.flows[i].values[j].value)}
-                                                        <span>
-                                                            <ArrowUpRightIcon
-                                                                className="w-4 h-4 mb-1"
-                                                                style={{ display: "inline" }} />
-                                                        </span>
-                                                    </a>
-                                                </span>
-                                                /{summary!.flows[i].values[j].name} {''}
-                                            </p>
-                                        );
-                                    })}
-                                </div>
-
+            {/* <SmallTitle title={'flows/' + summary?.flows.length} /> */}
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-8 py-8 mx-12'>
+                {Array.from({ length: summary?.flows.length as number }).map((object, i) => {
+                    return (
+                        <div key={i} className='border-2 border-lightgrey rounded-md'>
+                            <div className='pt-5 pl-5 text-lg border-b-0 border-white '>
+                                {summary!.flows[i].name}
                             </div>
-                        );
-                    })}
-                </ul>
-            </section>
+
+
+                            <div className="mx-12 py-8">
+                                {/* <div className="border-b-2 border-white text-xs ">
+                                    flows
+                                </div> */}
+
+                                {Array.from({ length: summary!.flows[i].values.length as number }).map((object, j) => {
+                                    return (
+                                        <div className='text-sm'
+                                        key={j}>
+                                            {j + 1}.{' '} 
+                                            <span>
+                                                <a target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    href={deploy.scan + summary!.flows[i].values[j].value} >
+                                                    {pretty(summary!.flows[i].values[j].value)}
+                                                    <span>
+                                                        <ArrowUpRightIcon
+                                                            className="w-4 h-4 mb-1"
+                                                            style={{ display: "inline" }} />
+                                                    </span>
+                                                </a>
+                                            </span>
+                                            /{summary!.flows[i].values[j].name} {''}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+
+                        </div>
+                    );
+                })}
+            </div>
         </section>
 
     );
